3f67341389beee20d1df0fecc2de86d1c1e915bd
David Lam <dkwlam@gmail.com>
2016-05-17 16:46:43 -0700

    WIP add functionality

---
+++ results/results_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('app.results module', function() {
+
+    beforeEach(module('app.results'));
+
+    describe('Results controller', function() {
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var view1Ctrl = $controller('Results');
+            expect(view1Ctrl).toBeDefined();
+        }));
+
+    });
+});
