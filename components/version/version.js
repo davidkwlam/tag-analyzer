7ef822d4c5479dd5fe464d671d2d7b454702646c
David Lam <dkwlam@gmail.com>
2016-05-17 12:21:28 -0700

    Clean up code and wrap JS in IIFE

--- components/version/version.js
+++ components/version/version.js
@@ -1,8 +1,13 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('myApp.version', [
-  'myApp.version.interpolate-filter',
-  'myApp.version.version-directive'
-])
+    angular
+        .module('myApp.version', [
+            'myApp.version.interpolate-filter',
+            'myApp.version.version-directive'
+        ]);
 
-.value('version', '0.1');
+    angular
+        .module('myApp.version')
+        .value('version', '0.1');
+})();
