0358f1eea843c40b67260e437ade513d9db88150
David Lam <dkwlam@gmail.com>
2016-05-18 15:01:34 -0700

    Adding tests

---
+++ app/services/parser_test.js
@@ -0,0 +1,161 @@
+'use strict';
+
+describe('Parser', function() {
+    var parser;
+
+    beforeEach(function() {
+        module('app');
+
+        inject(function($injector) {
+            parser = $injector.get("Parser");
+        });
+    });
+
+    it('check the existence of Parser factory', function() {
+        expect(parser).toBeDefined();
+    });
+
+    it('should return an error when given an empty string', function() {
+        var body = '';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBeDefined();
+        expect(tags).toBe(undefined);
+    });
+
+    it('should return an error when given a random string', function() {
+        var body = 'Blah blah blah blah blah';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBeDefined();
+        expect(tags).toBe(undefined);
+    });
+
+    it('should return an error when given CSS', function() {
+        var body = 'p { font-size: 12px; font-weight: bold; }';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBeDefined();
+        expect(tags).toBe(undefined);
+    });
+
+    it('should return an error when given javascript', function() {
+        var body = 'console.log("Hello World")';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBeDefined();
+        expect(tags).toBe(undefined);
+    });
+
+    it('should return an error when given malfomed XML', function() {
+        var body = '<sometag>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBeDefined();
+        expect(tags).toBe(undefined);
+    });
+
+    it('should return the correct tags for an XML document with one tag', function() {
+        var body = '<sometag></sometag>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBe(undefined);
+        expect(tags).toBeDefined();
+        expect(Object.keys(tags).length).toEqual(1);
+        expect(tags['sometag']).toEqual(1);
+    });
+
+    it('should return the correct tags and tag count for an XML document with two tags', function() {
+        var body = '<sometag><anothertag></anothertag></sometag>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBe(undefined);
+        expect(tags).toBeDefined();
+        expect(Object.keys(tags).length).toEqual(2);
+        expect(tags['sometag']).toEqual(1);
+        expect(tags['anothertag']).toEqual(1);
+    });
+
+    it('should return the correct tags and tag count for an XML document with two tags that are the same', function() {
+        var body = '<sometag><sometag></sometag></sometag>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBe(undefined);
+        expect(tags).toBeDefined();
+        expect(Object.keys(tags).length).toEqual(1);
+        expect(tags['sometag']).toEqual(2);
+    });
+
+    it('should return the correct tags and tag count for a simple HTML document', function() {
+        var body = '<!doctype HTML><html><head></head><body></body></html>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBe(undefined);
+        expect(tags).toBeDefined();
+        expect(Object.keys(tags).length).toEqual(3);
+        expect(tags['HTML']).toEqual(1);
+        expect(tags['HEAD']).toEqual(1);
+        expect(tags['BODY']).toEqual(1);
+    });
+
+    it('should return the correct tags and tag count for an HTML document with multiple tags', function() {
+        var body = '<!doctype HTML><html><head><title>This is a webpage</title></head><body><h1>Web Page</h1><p>Content</p></body></html>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBe(undefined);
+        expect(tags).toBeDefined();
+        expect(Object.keys(tags).length).toEqual(6);
+        expect(tags['HTML']).toEqual(1);
+        expect(tags['HEAD']).toEqual(1);
+        expect(tags['TITLE']).toEqual(1);
+        expect(tags['BODY']).toEqual(1);
+        expect(tags['H1']).toEqual(1);
+        expect(tags['P']).toEqual(1);
+    });
+
+    it('should return the correct tags and tag count for an HTML document with multiple duplicate tags', function() {
+        var body = '<!doctype HTML><html><head></head><body><h1>Web Page</h1><h1>Web Page Again</h1></body></html>';
+
+        var result = parser.getTags(body);
+        var error = result['error'];
+        var tags = result['tags'];
+
+        expect(error).toBe(undefined);
+        expect(tags).toBeDefined();
+        expect(Object.keys(tags).length).toEqual(4);
+        expect(tags['HTML']).toEqual(1);
+        expect(tags['HEAD']).toEqual(1);
+        expect(tags['BODY']).toEqual(1);
+        expect(tags['H1']).toEqual(2);
+    });
+});
---
+++ app/services/proxy_test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+describe('Parser', function() {
+    var proxy, CONSTANTS, $httpBackend;
+
+    beforeEach(function() {
+        module('app');
+
+        inject(function($injector) {
+            proxy = $injector.get("Proxy");
+            $httpBackend = $injector.get("$httpBackend");
+            CONSTANTS = $injector.get("CONSTANTS");
+        });
+    });
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('check the existence of Parser factory', function() {
+        expect(proxy).toBeDefined();
+    });
+
+    it('should call the proxy server with a given url', function() {
+        var url = 'www.google.com';
+        var data = '<html></html>';
+        var resultData;
+
+        var getOrdersRequestHandler = $httpBackend.when('GET', CONSTANTS.PROXY_SERVER + url)
+            .respond(data, {});
+
+        $httpBackend.expectGET(CONSTANTS.PROXY_SERVER + url);
+        proxy.get(url).then(function(result) {
+            resultData = result.data;
+        });
+        $httpBackend.flush();
+        
+        expect(resultData).toBe(data);
+    });
+});
